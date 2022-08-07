3c9442f9f49df8768a95e70afc9acdcc57b45169
Ruslan Zapolianenko <rusartrade@gmail.com>
2022-08-07 17:44:40 +0300

    Main first commit

---
+++ src/Testirovka.java
@@ -0,0 +1,12 @@
+public class Testirovka {
+    public static void main(String[] args) {
+
+        int a = 100;
+        int b = 3;
+        int rezult = ( a / b);
+        System.out.println(rezult);
+
+    }
+
+
+}
