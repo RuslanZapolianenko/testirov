6a16174cd36e582b86d400bdbda3c544e072f94b
Ruslan Zapolianenko <rusartrade@gmail.com>
2022-08-07 20:50:59 +0300

    Last Commit2 Branch_2

--- src/Brahch_2.java
+++ src/Brahch_2.java
@@ -2,6 +2,7 @@ public class Brahch_2 {
     public static void main(String[] args) {
 
         System.out.println("Изучаем разветление веток");
+        System.out.println("Пробуем конфликтную ситуацию");
 
 
 
